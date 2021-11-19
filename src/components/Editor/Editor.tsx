67735df6d9d65d573d99bf42716c2af5400b95a4
GulTion <gkv8192@gmail.com>
2021-11-19 11:32:37 +0530

    push

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -5,10 +5,11 @@ import './Editor.scss'
 
 export default function Header() {
   const [content, saveContent] =
-    React.useState('Using Python as backend, you can perform operations that are not allowed in Javascript, for example disk access. Click button below to save this content to hard drive.')
+    React.useState('Using Python as backenlowed in Javascript, for example disk access. Click button below to save this content to hard drive.')
 
   return (
     <div className='editor-container'>
+      Gulshans
       <textarea className='textarea' value={content} onChange={(e) => {
         saveContent(e.target.value)
       }}/><br/>
